03659add2a4dd370efedc4132e4a02635ed853b2
Eunhye Jeong <gracejelly125@gmail.com>
2024-11-22 16:34:52 +0900

    calculator 문제 풀이

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,6 +1,11 @@
 // TODO: store 에 slice에서 import한 reducer를 할당하세요.
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../slices/calculatorSlice";
 
-export default configureStore({
-  reducer: {},
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
 });
+
+export default store;
--- src/redux/slices/calculatorSlice.js
+++ src/redux/slices/calculatorSlice.js
@@ -3,29 +3,24 @@
 
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  number : 0,
+}
+
 export const counterSlice = createSlice({
   name: "counter",
-  initialState: {
-    value: 0,
-  },
+  initialState,
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
+    addNumber: (state, action) => {
+      state.number = state.number + action.payload;
     },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
-    },
+    removeNumber: (state, action) => {
+      state.number = state.number - action.payload;
+    }
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { addNumber, removeNumber } = counterSlice.actions;
 
 export default counterSlice.reducer;
